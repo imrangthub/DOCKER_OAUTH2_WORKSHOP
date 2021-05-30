2d040e99ab94d86559a35d19845899921e21412b
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2021-05-30 18:27:45 +0600

    Update resource

---
+++ GRANT_TYPE_PASSWORD/authorization-server-api/src/main/java/com/imranmadbar/AuthoriztionServerApplication.java
@@ -0,0 +1,15 @@
+package com.imranmadbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableResourceServer
+@SpringBootApplication
+public class AuthoriztionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthoriztionServerApplication.class, args);
+	}
+
+}
---
+++ GRANT_TYPE_PASSWORD/resource-server-api/src/main/java/com/imranmadbar/home/HomeController.java
@@ -0,0 +1,62 @@
+package com.imranmadbar.home;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.imranmadbar.util.Response;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class HomeController {
+
+	// @PreAuthorize("#oauth2.hasScope('read')")
+	
+	@GetMapping("/home")
+	public Response testMsg() {
+
+		System.out.println("From Admin Controller");
+
+		Authentication curretnAuthentication = SecurityContextHolder.getContext().getAuthentication();
+		System.out.println("Curretn Authentication All       ####: " + curretnAuthentication);
+		System.out.println("Curretn Authentication Name      ####: " + curretnAuthentication.getName());
+		System.out.println("Curretn Authentication Principal ####: " + curretnAuthentication.getPrincipal());
+
+		
+		Response res = new Response();
+		
+		res.setSuccess(true);
+		res.setMessage("From Resource server home Controller Get Mapping !");
+		
+		return res;
+	}
+	
+	
+	@PostMapping(value = "/home", produces = "application/json")
+	@ResponseBody
+    public Response getAll(@RequestBody(required = false) String reqObj) {
+		
+		System.out.println("From Admin Controller");
+
+		Authentication curretnAuthentication = SecurityContextHolder.getContext().getAuthentication();
+		System.out.println("Curretn Authentication All       ####: " + curretnAuthentication);
+		System.out.println("Curretn Authentication Name      ####: " + curretnAuthentication.getName());
+		System.out.println("Curretn Authentication Principal ####: " + curretnAuthentication.getPrincipal());
+		
+		//String res = "{}";
+		
+		
+		Response res = new Response();
+		
+		res.setSuccess(true);
+		res.setMessage("From Resource server home Controller post Mapping !");
+
+		return res;
+	}
+
+}
